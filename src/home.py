8c12542c3f773cefb6706f61c0860e92c17caac2
m39-ibrahim <mostafa2.ibrahim@live.uwe.ac.uk>
2024-01-16 23:03:56 +0000

    Navbar

--- src/home.py
+++ src/home.py
@@ -6,7 +6,7 @@ from flask import render_template, request, redirect
 @app.route("/index")
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("index.html")
 
 
 @app.route("/about_us")
