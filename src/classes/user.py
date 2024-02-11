280fd4e5924f47f3ff9f941855bc1b9485459a4d
m39-ibrahim <mostafa2.ibrahim@live.uwe.ac.uk>
2024-02-11 16:53:47 +0000

    home page

--- src/classes/user.py
+++ src/classes/user.py
@@ -38,3 +38,40 @@ class User(object):
 
     def set_address(self, value):
         self.__address = value
+
+
+    def add_user(self, id, name, email, phone_number, address):
+        if db.select(User(id)):
+            print("Error: Item already exists")
+        else:
+            self.name = name
+            self.__email = email
+            self.phone_number = phone_number
+            self.address = address
+            db.Insert(self)
+
+    def delete_user(self, id):
+        existing_user = db.Select(User(id))
+        if existing_user:
+            db.Delete(User(id))
+        else:
+            print("Error: Item not found")
+   
+
+        db.delete_user(self.get_id())
+
+def update_user(self, id, new_name=None, new_email=None, new_phone_number=None, new_address=None):
+    existing_user = db.Select(User(id))
+    if existing_user:
+        if new_name is not None:
+            existing_user[0]["name"] = new_name
+        if new_email is not None:
+            existing_user[0]["email"] = new_email
+        if new_phone_number is not None:
+            existing_user[0]["phone_number"] = new_phone_number
+        if new_address is not None:
+            existing_user[0]["address"] = new_address
+
+        db.update_user(existing_user[0])
+    else:
+        print("Error: User not found")
