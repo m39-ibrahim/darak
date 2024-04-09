8ab81d92e05542c5057cf556cde83d659e09d96a
m39-ibrahim <mostafa2.ibrahim@live.uwe.ac.uk>
2024-04-09 19:00:35 +0100

    Categories section title

--- src/static/script.js
+++ src/static/script.js
@@ -95,6 +95,12 @@ document.addEventListener('DOMContentLoaded', function() {
             const category = this.getAttribute('data-category');
             const categoryArray = category.split(', '); // Split category string into array
             categoryContainer.innerHTML = ''; // Clear previous content
+            // Create and append the title element
+            const titleElement = document.createElement('h2');
+            titleElement.textContent = 'Categories';
+            titleElement.classList.add('category-title');
+            categoryContainer.insertBefore(titleElement, categoryContainer.firstChild);
+            categoryContainer.style.paddingTop = '70px';
             categoryArray.forEach(function(cat) {
                 const categoryElement = document.createElement('a');
                 const d = document.createElement('div');
